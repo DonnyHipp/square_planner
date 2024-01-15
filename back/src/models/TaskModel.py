2424cf51018b72d2d67d6ddd5b218976b6cdc76f
DonnyHipp <toloknov.igor123@gmail.com>
2024-01-15 22:11:06 +0300

    feat:smart changes in frontend and add task sql models

--- back/src/models/TaskModel.py
+++ back/src/models/TaskModel.py
@@ -1,29 +1,41 @@
-from typing import Optional
-from sqlalchemy import ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, Enum, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+from enum import Enum as SQLAlchemyEnum
 
+Base = declarative_base()
 
-from sqlalchemy.orm import Mapped, mapped_column
-from enum import Enum
-
-
-from configs.db import Base
-
-
-class Quadro(Enum):
-    pass
-
-
-class Role(Base):
-    __tablename__ = 'role'
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[str]
-
+class Importance(str, SQLAlchemyEnum):
+    URGENT_IMPORTANT = "urgent_important"
+    URGENT_NOT_IMPORTANT = "urgent_not_important"
+    NOT_URGENT_IMPORTANT = "not_urgent_important"
+    NOT_URGENT_NOT_IMPORTANT = "not_urgent_not_important"
 
 class Task(Base):
-    __tablename__ = 'task'
+    __tablename__ = 'tasks'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    title:Mapped[str]
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True, nullable=False)
+    creation_date = Column(DateTime, default=datetime.utcnow)
+    update_date = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    deadline = Column(DateTime)
+    quadrant = Column(Enum(Importance), nullable=False)
+    role_id = Column(Integer, ForeignKey('roles.id'))
+    metric_id = Column(Integer, ForeignKey('metrics.id'))
 
+    role_name = Column(String)  # Добавляем mapped column для связи с ролью (без relationship)
+    metric_name = Column(String)  # Добавляем mapped column для связи с метрикой (без relationship)
 
+# Добавляем таблицы для ролей и метрик
+class Role(Base):
+    __tablename__ = 'roles'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True, nullable=False)
+
+class Metric(Base):
+    __tablename__ = 'metrics'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True, nullable=False)
